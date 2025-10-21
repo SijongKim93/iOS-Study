d7d9efadd03be3a006ed84a97ccae572fd7d78de
SijongKim93 <rlatlwhd456@gmail.com>
2025-10-21 16:31:17 +0900

    Initial Commit

---
+++ iOS-Study/iOS_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_StudyApp.swift
+//  iOS-Study
+//
+//  Created by duse on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
