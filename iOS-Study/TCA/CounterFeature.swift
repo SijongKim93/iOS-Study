4a5540a6da6d17017f0ef76d186e6e69fd3b097b
SijongKim93 <rlatlwhd456@gmail.com>
2025-10-24 17:12:24 +0900

    TCA Feature 간단 이해 자료 추가

--- iOS-Study/TCA/CounterFeature.swift
+++ iOS-Study/TCA/CounterFeature.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct CounterFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        var count = 0
+        var isLoading = false
+    }
+    
+    enum Action {
+        case incrementButtonTapped
+        case decrementButtonTapped
+        case resetButtonTapped
+        case loadingChanged(Bool)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+                
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+                
+            case .resetButtonTapped:
+                state.count = 0
+                return .none
+                
+            case let .loadingChanged(isLoading):
+                state.isLoading = isLoading
+                return .none
+            }
+        }
+    }
+}
