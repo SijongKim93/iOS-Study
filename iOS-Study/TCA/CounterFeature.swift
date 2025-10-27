1049dd7e02558e30b0e113fe985e2991e88b9cea
SijongKim93 <rlatlwhd456@gmail.com>
2025-10-27 18:04:33 +0900

    CountFeature 추가

--- iOS-Study/TCA/CounterFeature.swift
+++ iOS-Study/TCA/CounterFeature.swift
@@ -15,13 +15,20 @@ struct CounterFeature {
     struct State: Equatable {
         var count = 0
         var isLoading = false
+        var errorMessage: String?
+        var lastAction: String = ""
     }
     
     enum Action {
         case incrementButtonTapped
         case decrementButtonTapped
         case resetButtonTapped
-        case loadingChanged(Bool)
+        case doubleButtonTapped
+        case randomButtonTapped
+        case loadFromServer
+        case serverResponse(Int)
+        case serverError(String)
+        case clearError
     }
     
     var body: some ReducerOf<Self> {
@@ -29,18 +36,65 @@ struct CounterFeature {
             switch action {
             case .incrementButtonTapped:
                 state.count += 1
+                state.lastAction = "증가"
+                state.errorMessage = nil
                 return .none
                 
             case .decrementButtonTapped:
                 state.count -= 1
+                state.lastAction = "감소"
+                state.errorMessage = nil
                 return .none
                 
             case .resetButtonTapped:
                 state.count = 0
+                state.lastAction = "리셋"
+                state.errorMessage = nil
                 return .none
                 
-            case let .loadingChanged(isLoading):
-                state.isLoading = isLoading
+            case .doubleButtonTapped:
+                state.count *= 2
+                state.lastAction = "2배 증가"
+                state.errorMessage = nil
+                return .none
+                
+            case .randomButtonTapped:
+                state.count = Int.random(in: -100...100)
+                state.lastAction = "랜덤 값"
+                state.errorMessage = nil
+                return .none
+                
+            case .loadFromServer:
+                state.isLoading = true
+                state.errorMessage = nil
+                state.lastAction = "서버 로딩 중..."
+                // 비동기 작업 시뮬레이션
+                return .run { send in
+                    try await Task.sleep(for: .seconds(2))
+                    
+                    // 30% 확률로 에러 발생
+                    if Bool.random() {
+                        await send(.serverError("서버 연결 실패"))
+                    } else {
+                        let randomValue = Int.random(in: 1...50)
+                        await send(.serverResponse(randomValue))
+                    }
+                }
+                
+            case let .serverResponse(value):
+                state.count = value
+                state.isLoading = false
+                state.lastAction = "서버에서 로드됨: \(value)"
+                return .none
+                
+            case let .serverError(message):
+                state.isLoading = false
+                state.errorMessage = message
+                state.lastAction = "에러 발생"
+                return .none
+                
+            case .clearError:
+                state.errorMessage = nil
                 return .none
             }
         }
