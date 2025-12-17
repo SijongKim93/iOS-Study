34da7ae3a951596fe3672ab0086274106276e48f
SijongKim93 <rlatlwhd456@gmail.com>
2025-12-17 18:05:42 +0900

    WebView Navi 분기 추가

---
+++ iOS-Study/MiniProjects/WebView/CounterWebFeature.swift
@@ -0,0 +1,19 @@
+//
+//  CounterWebFeature.swift
+//  iOS-Study
+//
+//  Created by Sijong on 12/17/25.
+//
+
+import Foundation
+
+public enum NavigationStep {
+    case serviceTermsPopup(serviceType: String, onAgreeTerms: () -> Void)
+    case residentAuthentication
+    case map(title: String, latitude: Double, longitude: Double)
+    case messageRoom(roomID: String, nickname: String)
+    case myPointHistory
+    case commonWebView(title: String, url: String, isShowTitleBar: Bool)
+    case webView(url: String, headers: [String: String]?, service: String?)
+    case userProfile(memberIndex: String, isCommunity: Bool, departFrom: String)
+}
